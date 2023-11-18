10f8d1fd45a0d4464621074f24adc368afe75ad8
retrobanner <live@goml.ru>
2023-11-18 14:50:56 +0300

    add SmartDynamicArray and its tests

--- С++/3 semestre/lab1/lab1/src/data_classes/SmartDynamicArray.hpp
+++ С++/3 semestre/lab1/lab1/src/data_classes/SmartDynamicArray.hpp
@@ -19,29 +19,27 @@ namespace sem3 {
         SmartDynamicArray(const SmartDynamicArray &other);
         SmartDynamicArray(SmartDynamicArray &&other);
         SmartDynamicArray(const lab2::DynamicArray<T> &other);
-        SmartDynamicArray(lab2::DynamicArray<T> &&other);
 
         ~SmartDynamicArray() = default;
 
         SmartDynamicArray<T> &operator=(const SmartDynamicArray &other);
         SmartDynamicArray<T> &operator=(SmartDynamicArray &&other);
         SmartDynamicArray<T> &operator=(const lab2::DynamicArray<T> &other);
-        SmartDynamicArray<T> &operator=(lab2::DynamicArray<T> &&other);
 
         T get(int index) const;
         [[nodiscard]] int getSize() const;
-        SmartDynamicArray<T> &getSubArray(int startIndex, int endIndex) const;
+        SmartDynamicArray<T> getSubArray(int startIndex, int endIndex) const;
 
         void set(int index, const T &item);
         void set(int startIndex, int endIndex, const T &item);
 
         T &operator[](int index);
-        SmartDynamicArray<T> &operator+(const SmartDynamicArray &other) const;
+        SmartDynamicArray<T> operator+(const SmartDynamicArray<T> &other) const;
 
         void resize(int newSize);
         void resize(int newSize, const T &item);
 
-        SmartDynamicArray<T> &concat(const SmartDynamicArray<T> &other) const;
+        SmartDynamicArray<T> concat(const SmartDynamicArray<T> &other) const;
     };
 
     template <typename T>
@@ -94,6 +92,67 @@ namespace sem3 {
     }
 
     template <typename T>
+    SmartDynamicArray<T>::SmartDynamicArray(const SmartDynamicArray &other) {
+        size = other.size;
+        data = make_shared<T[]>(size);
+        for (int i = 0; i < size; i++) {
+            data[i] = other.data[i];
+        }
+    }
+
+    template <typename T>
+    SmartDynamicArray<T>::SmartDynamicArray(SmartDynamicArray &&other) {
+        size = other.size;
+        data = other.data;
+        other.size = 0;
+        other.data = nullptr;
+    }
+
+    template <typename T>
+    SmartDynamicArray<T>::SmartDynamicArray(const lab2::DynamicArray<T> &other) {
+        size = other.GetSize();
+        data = make_shared<T[]>(size);
+        for (int i = 0; i < size; i++) {
+            data[i] = other.Get(i);
+        }
+    }
+
+    template <typename T>
+    SmartDynamicArray<T> &SmartDynamicArray<T>::operator=(const SmartDynamicArray &other) {
+        if (this == &other) {
+            return *this;
+        }
+        size = other.size;
+        data = make_shared<T[]>(size);
+        for (int i = 0; i < size; i++) {
+            data[i] = other.data[i];
+        }
+        return *this;
+    }
+
+    template <typename T>
+    SmartDynamicArray<T> &SmartDynamicArray<T>::operator=(SmartDynamicArray &&other) {
+        if (this == &other) {
+            return *this;
+        }
+        size = other.size;
+        data = other.data;
+        other.size = 0;
+        other.data = nullptr;
+        return *this;
+    }
+
+    template <typename T>
+    SmartDynamicArray<T> &SmartDynamicArray<T>::operator=(const lab2::DynamicArray<T> &other) {
+        size = other.GetSize();
+        data = make_shared<T[]>(size);
+        for (int i = 0; i < size; i++) {
+            data[i] = other.Get(i);
+        }
+        return *this;
+    }
+
+    template <typename T>
     T SmartDynamicArray<T>::get(int index) const {
         if (index >= size or index < 0) {
             throw std::out_of_range("Index out of range");
@@ -108,12 +167,99 @@ namespace sem3 {
     }
 
     template <typename T>
+    SmartDynamicArray<T> SmartDynamicArray<T>::getSubArray(int startIndex, int endIndex) const {
+        if (startIndex < 0 or startIndex >= size or endIndex < 0 or endIndex > size) {
+            throw std::out_of_range("Index out of range");
+        } else if (startIndex > endIndex) {
+            throw std::invalid_argument("Start index must be less than end index");
+        } else {
+            int newSize = endIndex - startIndex;
+            SmartDynamicArray result(newSize);
+            for (int i = 0; i < newSize; i++) {
+                result.data[i] = data[startIndex + i];
+            }
+            return result;
+        }
+    }
+
+    template <typename T>
+    void SmartDynamicArray<T>::set(int index, const T &item) {
+        if (index >= size or index < 0) {
+            throw std::out_of_range("Index out of range");
+        } else {
+            data[index] = item;
+        }
+    }
+
+    template <typename T>
+    void SmartDynamicArray<T>::set(int startIndex, int endIndex, const T &item) {
+        if (startIndex < 0 or startIndex >= size or endIndex < 0 or endIndex > size) {
+            throw std::out_of_range("Index out of range");
+        } else if (startIndex > endIndex) {
+            throw std::invalid_argument("Start index must be less than end index");
+        } else {
+            for (int i = startIndex; i < endIndex; i++) {
+                data[i] = item;
+            }
+        }
+    }
+
+    template <typename T>
     T &SmartDynamicArray<T>::operator[](int index) {
         if (index >= size or index < 0) {
             throw std::out_of_range("Index out of range");
         } else {
             return data[index];
         }
+    }
+
+    template <typename T>
+    SmartDynamicArray<T> SmartDynamicArray<T>::operator+(const SmartDynamicArray<T> &other) const {
+        return concat(other);
+    }
+
+    template <typename T>
+    void SmartDynamicArray<T>::resize(int newSize) {
+        if (newSize < 0) {
+            throw std::invalid_argument("Size must be positive");
+        } else {
+            auto newData = make_shared<T[]>(newSize);
+            for (int i = 0; i < size and i < newSize; i++) {
+                newData[i] = data[i];
+            }
+            data = newData;
+            size = newSize;
+        }
+    }
+
+    template <typename T>
+    void SmartDynamicArray<T>::resize(int newSize, const T &item) {
+        if (newSize < 0) {
+            throw std::invalid_argument("Size must be positive");
+        } else {
+            auto newData = make_shared<T[]>(newSize);
+            for (int i = 0; i < size and i < newSize; i++) {
+                newData[i] = data[i];
+            }
+            for (int i = size; i < newSize; i++) {
+                newData[i] = item;
+            }
+            data = newData;
+            size = newSize;
+        }
+    }
+
+    template <typename T>
+    SmartDynamicArray<T> SmartDynamicArray<T>::concat(const SmartDynamicArray<T> &other) const {
+        int newSize = size + other.size;
+        SmartDynamicArray result(newSize);
+        for (int i = 0; i < size; i++) {
+            result.data[i] = data[i];
+        }
+        for (int i = 0; i < other.size; i++) {
+            result.data[size + i] = other.data[i];
+        }
+        return result;
     }
 
 } // namespace sem3
--- С++/3 semestre/lab1/lab1/tests/DataClasses_tests/SmartDynamicArray_tests.cpp
+++ С++/3 semestre/lab1/lab1/tests/DataClasses_tests/SmartDynamicArray_tests.cpp
@@ -153,3 +153,660 @@ TEST(SmartDynamicArray, ConstructorWithSharedPtrArray) {
     EXPECT_EQ(arr2.get(4), "TEST4");
 }
 
+TEST(SmartDynamicArray, ConstructorWithSharedPtrArrayWithZeroSize) {
+    // arrange
+    auto arr = sem3::make_shared<std::string[]>(0);
+
+    // act
+    SmartDynamicArray<std::string> arr2(0, arr);
+
+    // assert
+    EXPECT_EQ(arr2.getSize(), 0);
+    EXPECT_THROW(arr2.get(0), std::out_of_range);
+    EXPECT_THROW(arr2[0], std::out_of_range);
+}
+
+TEST(SmartDynamicArray, ConstructorWithSharedPtrArrayWithNegativeSize) {
+    // arrange
+    auto arr = sem3::make_shared<std::string[]>(0);
+
+    // act
+
+    // assert
+    EXPECT_THROW(SmartDynamicArray<std::string>(-1, arr), std::invalid_argument);
+}
+
+TEST(SmartDynamicArray, CopyConstructor) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+    SmartDynamicArray<std::string> arr2(*arr);
+
+    // assert
+    EXPECT_EQ(arr2.getSize(), 10);
+    EXPECT_EQ(arr2.get(0), "TEST0");
+    EXPECT_EQ(arr2.get(4), "TEST4");
+}
+
+TEST(SmartDynamicArray, CopyConstructorWithZeroSize) {
+    // arrange
+    auto arr = make_shared<SmartDynamicArray<int>>(0, 0);
+
+    // act
+    SmartDynamicArray<int> arr2(*arr);
+
+    // assert
+    EXPECT_EQ(arr2.getSize(), 0);
+    EXPECT_THROW(arr2.get(0), std::out_of_range);
+    EXPECT_THROW(arr2[0], std::out_of_range);
+}
+
+TEST(SmartDynamicArray, MoveConstructor) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+    SmartDynamicArray<std::string> arr2(std::move(*arr));
+
+    // assert
+    EXPECT_EQ(arr2.getSize(), 10);
+    EXPECT_EQ(arr2.get(0), "TEST0");
+    EXPECT_EQ(arr2.get(4), "TEST4");
+    EXPECT_EQ(arr->getSize(), 0);
+    EXPECT_THROW(arr->get(0), std::out_of_range);
+}
+
+TEST(SmartDynamicArray, MoveConstructorWithZeroSize) {
+    // arrange
+    auto arr = make_shared<SmartDynamicArray<int>>(0, 0);
+
+    // act
+    SmartDynamicArray<int> arr2(std::move(*arr));
+
+    // assert
+    EXPECT_EQ(arr2.getSize(), 0);
+    EXPECT_THROW(arr2.get(0), std::out_of_range);
+    EXPECT_THROW(arr2[0], std::out_of_range);
+    EXPECT_EQ(arr->getSize(), 0);
+    EXPECT_THROW(arr->get(0), std::out_of_range);
+}
+
+TEST(SmartDynamicArray, DynamicArrayCopyConstructor) {
+    // arrange
+    lab2::DynamicArray<int> arr(10);
+    for (int i = 0; i != 10; ++i) {
+        arr.Set(i, i);
+    }
+
+    // act
+    SmartDynamicArray<int> arr2(arr);
+
+    // assert
+    EXPECT_EQ(arr2.getSize(), 10);
+    EXPECT_EQ(arr2.get(0), 0);
+    EXPECT_EQ(arr2.get(4), 4);
+}
+
+TEST(SmartDynamicArray, DynamicArrayCopyConstructorWithZeroSize) {
+    // arrange
+    lab2::DynamicArray<int> arr(0);
+
+    // act
+    SmartDynamicArray<int> arr2(arr);
+
+    // assert
+    EXPECT_EQ(arr2.getSize(), 0);
+    EXPECT_THROW(arr2.get(0), std::out_of_range);
+    EXPECT_THROW(arr2[0], std::out_of_range);
+}
+
+TEST(SmartDynamicArray, Destructor) {
+    // arrange
+    auto arr = new SmartDynamicArray<int>(10);
+
+    // act
+    delete arr;
+
+    // assert
+}
+
+TEST(SmartDynamicArray, AssignmentOperator) {
+    // arrange
+    auto arr = ArrayForTest();
+    SmartDynamicArray<std::string> arr2;
+
+    // act
+    arr2 = *arr;
+
+    // assert
+    EXPECT_EQ(arr2.getSize(), 10);
+    EXPECT_EQ(arr2.get(0), "TEST0");
+    EXPECT_EQ(arr2.get(4), "TEST4");
+}
+
+TEST(SmartDynamicArray, AssignmentOperatorWithZeroSize) {
+    // arrange
+    auto arr = make_shared<SmartDynamicArray<int>>(0, 0);
+    SmartDynamicArray<int> arr2;
+
+    // act
+    arr2 = *arr;
+
+    // assert
+    EXPECT_EQ(arr2.getSize(), 0);
+    EXPECT_THROW(arr2.get(0), std::out_of_range);
+    EXPECT_THROW(arr2[0], std::out_of_range);
+}
+
+TEST(SmartDynamicArray, AssignmentOperatorWithSelf) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+    *arr = *arr;
+
+    // assert
+    EXPECT_EQ(arr->getSize(), 10);
+    EXPECT_EQ(arr->get(0), "TEST0");
+    EXPECT_EQ(arr->get(4), "TEST4");
+}
+
+TEST(SmartDynamicArray, MoveAssignmentOperator) {
+    // arrange
+    auto arr = ArrayForTest();
+    SmartDynamicArray<std::string> arr2;
+
+    // act
+    arr2 = std::move(*arr);
+
+    // assert
+    EXPECT_EQ(arr2.getSize(), 10);
+    EXPECT_EQ(arr2.get(0), "TEST0");
+    EXPECT_EQ(arr2.get(4), "TEST4");
+    EXPECT_EQ(arr->getSize(), 0);
+    EXPECT_THROW(arr->get(0), std::out_of_range);
+}
+
+TEST(SmartDynamicArray, MoveAssignmentOperatorWithZeroSize) {
+    // arrange
+    auto arr = make_shared<SmartDynamicArray<int>>(0, 0);
+    SmartDynamicArray<int> arr2;
+
+    // act
+    arr2 = std::move(*arr);
+
+    // assert
+    EXPECT_EQ(arr2.getSize(), 0);
+    EXPECT_THROW(arr2.get(0), std::out_of_range);
+    EXPECT_THROW(arr2[0], std::out_of_range);
+    EXPECT_EQ(arr->getSize(), 0);
+    EXPECT_THROW(arr->get(0), std::out_of_range);
+}
+
+TEST(SmartDynamicArray, MoveAssignmentOperatorWithSelf) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+    *arr = std::move(*arr);
+
+    // assert
+    EXPECT_EQ(arr->getSize(), 10);
+    EXPECT_EQ(arr->get(0), "TEST0");
+    EXPECT_EQ(arr->get(4), "TEST4");
+}
+
+TEST(SmartDynamicArray, DynamicArrayAssignmentOperator) {
+    // arrange
+    lab2::DynamicArray<int> arr(10);
+    for (int i = 0; i != 10; ++i) {
+        arr.Set(i, i);
+    }
+    SmartDynamicArray<int> arr2;
+
+    // act
+    arr2 = arr;
+
+    // assert
+    EXPECT_EQ(arr2.getSize(), 10);
+    EXPECT_EQ(arr2.get(0), 0);
+    EXPECT_EQ(arr2.get(4), 4);
+}
+
+TEST(SmartDynamicArray, DynamicArrayAssignmentOperatorWithZeroSize) {
+    // arrange
+    lab2::DynamicArray<int> arr(0);
+    SmartDynamicArray<int> arr2;
+
+    // act
+    arr2 = arr;
+
+    // assert
+    EXPECT_EQ(arr2.getSize(), 0);
+    EXPECT_THROW(arr2.get(0), std::out_of_range);
+    EXPECT_THROW(arr2[0], std::out_of_range);
+}
+
+TEST(SmartDynamicArray, GetMethod) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+
+    // assert
+    EXPECT_EQ(arr->get(0), "TEST0");
+    EXPECT_EQ(arr->get(4), "TEST4");
+    EXPECT_EQ(arr->get(9), "TEST9");
+}
+
+TEST(SmartDynamicArray, GetMethodWithZeroSize) {
+    // arrange
+    auto arr = make_shared<SmartDynamicArray<int>>(0, 0);
+
+    // act
+
+    // assert
+    EXPECT_THROW(arr->get(0), std::out_of_range);
+    EXPECT_THROW((*arr)[0], std::out_of_range);
+}
+
+TEST(SmartDynamicArray, GetMethodWithNegativeIndex) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+
+    // assert
+    EXPECT_THROW(arr->get(-1), std::out_of_range);
+    EXPECT_THROW((*arr)[-1], std::out_of_range);
+}
+
+TEST(SmartDynamicArray, GetMethodWithIndexOutOfRange) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+
+    // assert
+    EXPECT_THROW(arr->get(10), std::out_of_range);
+    EXPECT_THROW((*arr)[10], std::out_of_range);
+}
+
+TEST(SmartDynamicArray, GetSizeMethod) {
+    // arrange
+    auto arr = ArrayForTest();
+    auto arr2 = make_shared<SmartDynamicArray<int>>(0, 0);
+
+    // act
+
+    // assert
+    EXPECT_EQ(arr->getSize(), 10);
+    EXPECT_EQ(arr2->getSize(), 0);
+}
+
+TEST(SmartDynamicArray, GetSubArrayMethod) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+    auto arr2 = arr->getSubArray(2, 5);
+    auto arr3 = arr->getSubArray(0, 10);
+
+    // assert
+    EXPECT_EQ(arr2.get(0), "TEST2");
+    EXPECT_EQ(arr2.getSize(), 3);
+    EXPECT_EQ(arr2.get(2), "TEST4");
+    EXPECT_EQ(arr3.getSize(), 10);
+    EXPECT_EQ(arr3.get(0), "TEST0");
+    EXPECT_EQ(arr3.get(9), "TEST9");
+}
+
+TEST(SmartDynamicArray, GetSubArrayMethodExcepts) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+
+    // assert
+    EXPECT_THROW(arr->getSubArray(-1, 5), std::out_of_range);
+    EXPECT_THROW(arr->getSubArray(2, 1), std::invalid_argument);
+    EXPECT_THROW(arr->getSubArray(0, 11), std::out_of_range);
+    EXPECT_THROW(arr->getSubArray(10, 10), std::out_of_range);
+    EXPECT_THROW(arr->getSubArray(2, -1), std::out_of_range);
+}
+
+TEST(SmartDynamicArray, SetMethod) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+    arr->set(0, "TEST10");
+    arr->set(4, "TEST11");
+    arr->set(9, "TEST12");
+
+    // assert
+    EXPECT_EQ(arr->get(0), "TEST10");
+    EXPECT_EQ(arr->get(4), "TEST11");
+    EXPECT_EQ(arr->get(9), "TEST12");
+    EXPECT_EQ(arr->get(1), "TEST1");
+    EXPECT_EQ(arr->get(3), "TEST3");
+    EXPECT_EQ(arr->get(8), "TEST8");
+}
+
+TEST(SmartDynamicArray, SetMethodExceptions) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+
+    // assert
+    EXPECT_THROW(arr->set(-1, "TEST10"), std::out_of_range);
+    EXPECT_THROW(arr->set(10, "TEST10"), std::out_of_range);
+}
+
+TEST(SmartDynamicArray, OtherSetMethod) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+    arr->set(2, 5, "TEST10");
+
+    // assert
+    EXPECT_EQ(arr->get(0), "TEST0");
+    EXPECT_EQ(arr->get(1), "TEST1");
+    EXPECT_EQ(arr->get(2), "TEST10");
+    EXPECT_EQ(arr->get(3), "TEST10");
+    EXPECT_EQ(arr->get(4), "TEST10");
+    EXPECT_EQ(arr->get(5), "TEST5");
+    EXPECT_EQ(arr->get(6), "TEST6");
+    EXPECT_EQ(arr->get(9), "TEST9");
+}
+
+TEST(SmartDynamicArray, OtherSetMethodExceptions) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+
+    // assert
+    EXPECT_THROW(arr->set(-1, 5, "TEST10"), std::out_of_range);
+    EXPECT_THROW(arr->set(2, 1, "TEST10"), std::invalid_argument);
+    EXPECT_THROW(arr->set(0, 11, "TEST10"), std::out_of_range);
+    EXPECT_THROW(arr->set(10, 10, "TEST10"), std::out_of_range);
+    EXPECT_THROW(arr->set(2, -1, "TEST10"), std::out_of_range);
+}
+
+TEST(SmartDynamicArray, IndexOperator) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+    (*arr)[3] = "TEST10";
+
+    // assert
+    EXPECT_EQ((*arr)[0], "TEST0");
+    EXPECT_EQ((*arr)[4], "TEST4");
+    EXPECT_EQ((*arr)[9], "TEST9");
+    EXPECT_EQ((*arr)[3], "TEST10");
+}
+
+TEST(SmartDynamicArray, IndexOperatorWithZeroSize) {
+    // arrange
+    auto arr = make_shared<SmartDynamicArray<int>>(0, 0);
+
+    // act
+
+    // assert
+    EXPECT_THROW((*arr)[0], std::out_of_range);
+}
+
+TEST(SmartDynamicArray, IndexOperatorWithNegativeIndex) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+
+    // assert
+    EXPECT_THROW((*arr)[-1], std::out_of_range);
+}
+
+TEST(SmartDynamicArray, IndexOperatorWithIndexOutOfRange) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+
+    // assert
+    EXPECT_THROW((*arr)[10], std::out_of_range);
+}
+
+TEST(SmartDynamicArray, ConcatOperator) {
+    // arrange
+    auto arr = ArrayForTest();
+    auto arr2 = ArrayForTest();
+
+    // act
+    auto arr3 = *arr + *arr2;
+
+    // assert
+    EXPECT_EQ(arr3.getSize(), 20);
+    EXPECT_EQ(arr3.get(0), "TEST0");
+    EXPECT_EQ(arr3.get(4), "TEST4");
+    EXPECT_EQ(arr3.get(9), "TEST9");
+    EXPECT_EQ(arr3.get(10), "TEST0");
+    EXPECT_EQ(arr3.get(14), "TEST4");
+    EXPECT_EQ(arr3.get(19), "TEST9");
+}
+
+TEST(SmartDynamicArray, ManyConcatOperators) {
+    // arrange
+    auto arr = ArrayForTest();
+    auto arr2 = ArrayForTest();
+    auto arr3 = ArrayForTest();
+    auto arr4 = ArrayForTest();
+    auto arr5 = ArrayForTest();
+
+    // act
+    auto arr6 = *arr + *arr2 + *arr3 + *arr4 + *arr5;
+
+    // assert
+    EXPECT_EQ(arr6.getSize(), 50);
+    EXPECT_EQ(arr6.get(0), "TEST0");
+    EXPECT_EQ(arr6.get(4), "TEST4");
+    EXPECT_EQ(arr6.get(9), "TEST9");
+    EXPECT_EQ(arr6.get(10), "TEST0");
+    EXPECT_EQ(arr6.get(14), "TEST4");
+    EXPECT_EQ(arr6.get(19), "TEST9");
+    EXPECT_EQ(arr6.get(20), "TEST0");
+    EXPECT_EQ(arr6.get(24), "TEST4");
+    EXPECT_EQ(arr6.get(29), "TEST9");
+    EXPECT_EQ(arr6.get(30), "TEST0");
+    EXPECT_EQ(arr6.get(34), "TEST4");
+    EXPECT_EQ(arr6.get(39), "TEST9");
+    EXPECT_EQ(arr6.get(40), "TEST0");
+    EXPECT_EQ(arr6.get(44), "TEST4");
+    EXPECT_EQ(arr6.get(49), "TEST9");
+}
+
+TEST(SmartDynamicArray, ConcatOperatorWithZeroSize) {
+    // arrange
+    auto arr = make_shared<SmartDynamicArray<int>>(0, 0);
+    auto arr2 = make_shared<SmartDynamicArray<int>>(0, 0);
+
+    // act
+    auto arr3 = *arr + *arr2;
+
+    // assert
+    EXPECT_EQ(arr3.getSize(), 0);
+    EXPECT_THROW(arr3.get(0), std::out_of_range);
+}
+
+TEST(SmartDynamicArray, ConcatOperatorWithSelf) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+    auto arr3 = *arr + *arr;
+
+    // assert
+    EXPECT_EQ(arr3.getSize(), 20);
+    EXPECT_EQ(arr3.get(0), "TEST0");
+    EXPECT_EQ(arr3.get(4), "TEST4");
+    EXPECT_EQ(arr3.get(9), "TEST9");
+    EXPECT_EQ(arr3.get(10), "TEST0");
+    EXPECT_EQ(arr3.get(14), "TEST4");
+    EXPECT_EQ(arr3.get(19), "TEST9");
+}
+
+TEST(SmartDynamicArray, ResizeMethod) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+    arr->resize(5);
+
+    // assert
+    EXPECT_EQ(arr->getSize(), 5);
+    EXPECT_EQ(arr->get(0), "TEST0");
+    EXPECT_EQ(arr->get(4), "TEST4");
+    EXPECT_THROW(arr->get(5), std::out_of_range);
+}
+
+TEST(SmartDynamicArray, ResizeMethod2) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+    arr->resize(15);
+
+    // assert
+    EXPECT_EQ(arr->getSize(), 15);
+    EXPECT_EQ(arr->get(0), "TEST0");
+    EXPECT_EQ(arr->get(4), "TEST4");
+    EXPECT_EQ(arr->get(9), "TEST9");
+    EXPECT_THROW(arr->get(15), std::out_of_range);
+}
+
+TEST(SmartDynamicArray, ResizeMethodWithZeroSize) {
+    // arrange
+    auto arr = make_shared<SmartDynamicArray<int>>(0, 0);
+
+    // act
+    arr->resize(5);
+
+    // assert
+    EXPECT_EQ(arr->getSize(), 5);
+}
+
+TEST(SmartDynamicArray, ResizeMethodWithNegativeSize) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+
+    // assert
+    EXPECT_THROW(arr->resize(-1), std::invalid_argument);
+}
+
+TEST(SmartDynamicArray, ResizeMethodWithItem) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+    arr->resize(5, "TEST10");
+
+    // assert
+    EXPECT_EQ(arr->getSize(), 5);
+    EXPECT_EQ(arr->get(0), "TEST0");
+    EXPECT_EQ(arr->get(4), "TEST4");
+    EXPECT_THROW(arr->get(5), std::out_of_range);
+}
+
+TEST(SmartDynamicArray, ResizeMethodWithItem2) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+    arr->resize(15, "TEST10");
+
+    // assert
+    EXPECT_EQ(arr->getSize(), 15);
+    EXPECT_EQ(arr->get(0), "TEST0");
+    EXPECT_EQ(arr->get(4), "TEST4");
+    EXPECT_EQ(arr->get(9), "TEST9");
+    EXPECT_EQ(arr->get(10), "TEST10");
+    EXPECT_EQ(arr->get(14), "TEST10");
+    EXPECT_THROW(arr->get(15), std::out_of_range);
+}
+
+TEST(SmartDynamicArray, ResizeMethodWithItemWithZeroSize) {
+    // arrange
+    auto arr = make_shared<SmartDynamicArray<int>>(0, 0);
+
+    // act
+    arr->resize(5, 10);
+
+    // assert
+    EXPECT_EQ(arr->getSize(), 5);
+    EXPECT_EQ(arr->get(0), 10);
+    EXPECT_EQ(arr->get(4), 10);
+}
+
+TEST(SmartDynamicArray, ResizeMethodWithItemWithNegativeSize) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+
+    // assert
+    EXPECT_THROW(arr->resize(-1, "TEST10"), std::invalid_argument);
+}
+
+TEST(SmartDynamicArray, ConcatMethod) {
+    // arrange
+    auto arr = ArrayForTest();
+    auto arr2 = ArrayForTest();
+
+    // act
+    auto arr3 = arr->concat(*arr2);
+
+    // assert
+    EXPECT_EQ(arr3.getSize(), 20);
+    EXPECT_EQ(arr3.get(0), "TEST0");
+    EXPECT_EQ(arr3.get(4), "TEST4");
+    EXPECT_EQ(arr3.get(9), "TEST9");
+    EXPECT_EQ(arr3.get(10), "TEST0");
+    EXPECT_EQ(arr3.get(14), "TEST4");
+    EXPECT_EQ(arr3.get(19), "TEST9");
+}
+
+TEST(SmartDynamicArray, ConcatMethodWithZeroSize) {
+    // arrange
+    auto arr = make_shared<SmartDynamicArray<int>>(0, 0);
+    auto arr2 = make_shared<SmartDynamicArray<int>>(0, 0);
+
+    // act
+    auto arr3 = arr->concat(*arr2);
+
+    // assert
+    EXPECT_EQ(arr3.getSize(), 0);
+    EXPECT_THROW(arr3.get(0), std::out_of_range);
+}
+
+TEST(SmartDynamicArray, ConcatMethodWithSelf) {
+    // arrange
+    auto arr = ArrayForTest();
+
+    // act
+    auto arr3 = arr->concat(*arr);
+
+    // assert
+    EXPECT_EQ(arr3.getSize(), 20);
+    EXPECT_EQ(arr3.get(0), "TEST0");
+    EXPECT_EQ(arr3.get(4), "TEST4");
+    EXPECT_EQ(arr3.get(9), "TEST9");
+    EXPECT_EQ(arr3.get(10), "TEST0");
+    EXPECT_EQ(arr3.get(14), "TEST4");
+    EXPECT_EQ(arr3.get(19), "TEST9");
+}
