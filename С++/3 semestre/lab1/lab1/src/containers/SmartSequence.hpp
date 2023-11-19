567dcd5d063b625dbaf8cedcc8ac6f2076369346
retrobanner <live@goml.ru>
2023-11-19 23:30:16 +0300

    Fix destructor in SmartSequence class

--- С++/3 semestre/lab1/lab1/src/containers/SmartSequence.hpp
+++ С++/3 semestre/lab1/lab1/src/containers/SmartSequence.hpp
@@ -10,7 +10,7 @@ namespace sem3 {
     template <typename T>
     class SmartSequence {
     public:
-        ~SmartSequence<T>() = default;
+        ~SmartSequence() = default;
 
         T getFirst() const;
         T getLast() const;
