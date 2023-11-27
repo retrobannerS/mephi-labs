3099837828f3f7dc975180497bf1977d4441b3d3
retrobanner <live@goml.ru>
2023-11-27 23:20:53 +0300

    Add Quick and Shell sorters & tests

---
+++ С++/3 semestre/lab2/tests/QuickSorter_tests.cpp
@@ -0,0 +1,114 @@
+#include "QuickSorter.hpp"
+#include "SmartArraySequence.hpp"
+#include "gtest/gtest.h"
+#include <iostream>
+
+using namespace sem3;
+
+TEST(QuickSorter, Random) {
+    // arrange
+    SharedPtr<SmartSequence<int>> sequence = SharedPtr<SmartSequence<int>>(new SmartArraySequence<int>());
+    for (int i = 0; i < 4; ++i) {
+        sequence->append(rand() % 100);
+    }
+    QuickSorter<int> sorter(sequence, [](int a, int b) { return a - b; });
+
+    // act
+    sorter.sort();
+
+    // assert
+    for (int i = 0; i < sequence->getSize() - 1; ++i) {
+        EXPECT_TRUE(sequence->get(i) <= sequence->get(i + 1));
+    }
+}
+
+TEST(QuickSorter, Sorted) {
+    // arrange
+    SharedPtr<SmartSequence<int>> sequence = SharedPtr<SmartSequence<int>>(new SmartArraySequence<int>());
+    for (int i = 0; i < 10; ++i) {
+        sequence->append(i);
+    }
+    QuickSorter<int> sorter(sequence, [](int a, int b) { return a - b; });
+
+    // act
+    sorter.sort();
+
+    // assert
+    for (int i = 0; i < sequence->getSize() - 1; ++i) {
+        EXPECT_TRUE(sequence->get(i) <= sequence->get(i + 1));
+    }
+}
+
+TEST(QuickSorter, Reversed) {
+    // arrange
+    SharedPtr<SmartSequence<int>> sequence = SharedPtr<SmartSequence<int>>(new SmartArraySequence<int>());
+    for (int i = 10; i > 0; --i) {
+        sequence->append(i);
+    }
+    QuickSorter<int> sorter(sequence, [](int a, int b) { return a - b; });
+
+    // act
+    sorter.sort();
+
+    // assert
+    for (int i = 0; i < sequence->getSize() - 1; ++i) {
+        EXPECT_TRUE(sequence->get(i) <= sequence->get(i + 1));
+    }
+}
+
+TEST(QuickSorter, Same) {
+    // arrange
+    SharedPtr<SmartSequence<int>> sequence = SharedPtr<SmartSequence<int>>(new SmartArraySequence<int>());
+    for (int i = 0; i < 10; ++i) {
+        sequence->append(1);
+    }
+    QuickSorter<int> sorter(sequence, [](int a, int b) { return a - b; });
+
+    // act
+    sorter.sort();
+
+    // assert
+    for (int i = 0; i < sequence->getSize() - 1; ++i) {
+        EXPECT_TRUE(sequence->get(i) <= sequence->get(i + 1));
+    }
+}
+
+TEST(QuickSorter, Empty) {
+    // arrange
+    SharedPtr<SmartSequence<int>> sequence = SharedPtr<SmartSequence<int>>(new SmartArraySequence<int>());
+    QuickSorter<int> sorter(sequence, [](int a, int b) { return a - b; });
+
+    // act
+    sorter.sort();
+
+    // assert
+    EXPECT_EQ(sequence->getSize(), 0);
+}
+
+TEST(QuickSorter, One) {
+    // arrange
+    SharedPtr<SmartSequence<int>> sequence = SharedPtr<SmartSequence<int>>(new SmartArraySequence<int>());
+    sequence->append(1);
+    QuickSorter<int> sorter(sequence, [](int a, int b) { return a - b; });
+
+    // act
+    sorter.sort();
+
+    // assert
+    EXPECT_EQ(sequence->getSize(), 1);
+}
+
+TEST(QuickSorter, Two) {
+    // arrange
+    SharedPtr<SmartSequence<int>> sequence = SharedPtr<SmartSequence<int>>(new SmartArraySequence<int>());
+    sequence->append(2);
+    sequence->append(1);
+    QuickSorter<int> sorter(sequence, [](int a, int b) { return a - b; });
+
+    // act
+    sorter.sort();
+
+    // assert
+    EXPECT_EQ(sequence->getSize(), 2);
+    EXPECT_TRUE(sequence->get(0) <= sequence->get(1));
+}
---
+++ С++/3 semestre/lab2/tests/ShellSorter_tests.cpp
@@ -0,0 +1,114 @@
+#include "ShellSorter.hpp"
+#include "SmartArraySequence.hpp"
+#include "gtest/gtest.h"
+#include <iostream>
+
+using namespace sem3;
+
+TEST(ShellSorter, Random) {
+    // arrange
+    SharedPtr<SmartSequence<int>> sequence = SharedPtr<SmartSequence<int>>(new SmartArraySequence<int>());
+    for (int i = 0; i < 10; ++i) {
+        sequence->append(rand() % 100);
+    }
+    ShellSorter<int> sorter(sequence, [](int a, int b) { return a - b; });
+
+    // act
+    sorter.sort();
+
+    // assert
+    for (int i = 0; i < sequence->getSize() - 1; ++i) {
+        EXPECT_TRUE(sequence->get(i) <= sequence->get(i + 1));
+    }
+}
+
+TEST(ShellSorter, Sorted) {
+    // arrange
+    SharedPtr<SmartSequence<int>> sequence = SharedPtr<SmartSequence<int>>(new SmartArraySequence<int>());
+    for (int i = 0; i < 10; ++i) {
+        sequence->append(i);
+    }
+    ShellSorter<int> sorter(sequence, [](int a, int b) { return a - b; });
+
+    // act
+    sorter.sort();
+
+    // assert
+    for (int i = 0; i < sequence->getSize() - 1; ++i) {
+        EXPECT_TRUE(sequence->get(i) <= sequence->get(i + 1));
+    }
+}
+
+TEST(ShellSorter, Reversed) {
+    // arrange
+    SharedPtr<SmartSequence<int>> sequence = SharedPtr<SmartSequence<int>>(new SmartArraySequence<int>());
+    for (int i = 10; i > 0; --i) {
+        sequence->append(i);
+    }
+    ShellSorter<int> sorter(sequence, [](int a, int b) { return a - b; });
+
+    // act
+    sorter.sort();
+
+    // assert
+    for (int i = 0; i < sequence->getSize() - 1; ++i) {
+        EXPECT_TRUE(sequence->get(i) <= sequence->get(i + 1));
+    }
+}
+
+TEST(ShellSorter, Same) {
+    // arrange
+    SharedPtr<SmartSequence<int>> sequence = SharedPtr<SmartSequence<int>>(new SmartArraySequence<int>());
+    for (int i = 0; i < 10; ++i) {
+        sequence->append(1);
+    }
+    ShellSorter<int> sorter(sequence, [](int a, int b) { return a - b; });
+
+    // act
+    sorter.sort();
+
+    // assert
+    for (int i = 0; i < sequence->getSize() - 1; ++i) {
+        EXPECT_TRUE(sequence->get(i) <= sequence->get(i + 1));
+    }
+}
+
+TEST(ShellSorter, Empty) {
+    // arrange
+    SharedPtr<SmartSequence<int>> sequence = SharedPtr<SmartSequence<int>>(new SmartArraySequence<int>());
+    ShellSorter<int> sorter(sequence, [](int a, int b) { return a - b; });
+
+    // act
+    sorter.sort();
+
+    // assert
+    EXPECT_EQ(sequence->getSize(), 0);
+}
+
+TEST(ShellSorter, One) {
+    // arrange
+    SharedPtr<SmartSequence<int>> sequence = SharedPtr<SmartSequence<int>>(new SmartArraySequence<int>());
+    sequence->append(1);
+    ShellSorter<int> sorter(sequence, [](int a, int b) { return a - b; });
+
+    // act
+    sorter.sort();
+
+    // assert
+    EXPECT_EQ(sequence->getSize(), 1);
+}
+
+TEST(ShellSorter, Two) {
+    // arrange
+    SharedPtr<SmartSequence<int>> sequence = SharedPtr<SmartSequence<int>>(new SmartArraySequence<int>());
+    sequence->append(2);
+    sequence->append(1);
+    ShellSorter<int> sorter(sequence, [](int a, int b) { return a - b; });
+
+    // act
+    sorter.sort();
+
+    // assert
+    EXPECT_EQ(sequence->getSize(), 2);
+    EXPECT_TRUE(sequence->get(0) <= sequence->get(1));
+}
